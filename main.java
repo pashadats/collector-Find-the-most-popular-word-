729ab200356e96fab900630e846478802723cd62
pashadats <47777009+pashadats@users.noreply.github.com>
2019-11-26 00:28:04 +0300

    Add files via upload

--- main.java
+++ main.java
@@ -1,33 +1,52 @@
 
+import java.io.BufferedReader;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Scanner;
+import java.util.TreeMap;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
+import javax.swing.text.Utilities;
+
 
 public class main {
-	static void solution(String text)
-	{
-		String input = text;
-	    String word = Arrays.stream(input.split(" "))
-	            .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
-	            .entrySet().stream()
-	            .max(Map.Entry.comparingByValue())
-	            .map(Map.Entry::getKey)
-	            .orElse(null);
-	    System.out.println("Most popular word is " + word);
-	}
+
 	
-	public static void main(String[] args) throws IOException
+	private static Map<String, Long> newMap = new HashMap<>();
+	
+	public static void main(String[] args) throws FileNotFoundException, IOException 
 	{
-		FileInputStream inFile = new FileInputStream("D:\\filename.txt");
-		byte[] str = new byte[inFile.available()];
-		inFile.read(str);
-		String text = new String(str);
 		
-		solution(text);
+		final String path = "D:\\filename.txt";
+        try (BufferedReader reader = new BufferedReader(new FileReader(path))) {
+            reader
+                    .lines()
+                    .flatMap(s -> Arrays.stream(s.split(" ")))
+                    .forEach(s -> {
+                    	newMap.compute(s, (key, value) -> (value != null) ? value + 1L : 1L);
+                    });
+        }
+
+        Map<String, Long> result = newMap
+                .entrySet()
+                .stream()
+                .sorted(Comparator.comparingLong(Map.Entry::getValue))
+                .limit(10)
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+
+        System.out.println(result);
+		
+	}	
+       
 	}
-}
+
 
